ed013593f78e2ef6a7f4feddb0ad7fe89fe655b1
Mike Taylor <mike@whatsmycut.com>
2016-11-28 15:20:36 -0600

    runtime error

--- main.py
+++ main.py
@@ -55,21 +55,21 @@ def userprofile(username=None):
     """Return a friendly HTTP greeting."""
     error = None
 #    try:
-	retarray = [{
-            'formaction': request.args.get('action', ''),
-            'email': request.args.get('email', ''),
-            'first_name': request.args.get('first_name', ''),
-            'last_name': request.args.get('last_name', ''),
-            'password': request.args.get('password', ''),
-            'organization': request.args.get('organization', ''),
-            'designation': request.args.get('designation', ''),
-            'location_latitude': request.args.get('location_latitude', ''),
-            'location_longitude': request.args.get('location_longitude', ''),
-            'location': [{'lat': request.args.get('location_latitude'), 'lon': request.args.get('location_longitude', '')}],
-            'profile_picture': request.args.get('profile_picture', ''),
-            'contacts': request.args.get('contacts', '')
-        }];
-        return render_template('output.html', data=retarray)
+    retarray = [{
+        'formaction': request.args.get('action', ''),
+        'email': request.args.get('email', ''),
+        'first_name': request.args.get('first_name', ''),
+        'last_name': request.args.get('last_name', ''),
+        'password': request.args.get('password', ''),
+        'organization': request.args.get('organization', ''),
+        'designation': request.args.get('designation', ''),
+        'location_latitude': request.args.get('location_latitude', ''),
+        'location_longitude': request.args.get('location_longitude', ''),
+        'location': [{'lat': request.args.get('location_latitude'), 'lon': request.args.get('location_longitude', '')}],
+        'profile_picture': request.args.get('profile_picture', ''),
+        'contacts': request.args.get('contacts', '')
+    }];
+    return render_template('output.html', data=retarray)
 #    except KeyError as identifier:
 #        error = "FormError: " + identifier.message
 #    	return render_template('error.html', error=error)
