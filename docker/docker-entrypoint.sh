fb65cf8a42dc4b397a7bd15039c9d6412c73c17d
Mike Taylor <mike@whatsmycut.com>
2019-07-16 00:15:07 -0700

    App running! Needs data.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# install the Python and mysql components
-sudo apt-get -qq -y install bash python-dev python-pip python-virtualenv libmysqlclient-dev mysql-server
-
-# install GCP components
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo apt-get -qq -y --force-yes install google-cloud-sdk google-cloud-sdk-app-engine-python google-cloud-sdk-app-engine-python-extras
+# # install GCP components
+. env/bin/activate
+apt-get -qq -y install apt-transport-https ca-certificates
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+apt-get update && apt-get -qq -y --force-yes install google-cloud-sdk google-cloud-sdk-app-engine-python google-cloud-sdk-app-engine-python-extras google-cloud-sdk-datastore-emulator
+pip install --upgrade MySQL-python
+pip install --upgrade -r requirements.txt -t lib
+export CLOUDSDK_PYTHON=/usr/bin/python
 # sudo apt-get -qq -y install google-cloud-sdk google-cloud-sdk-app-engine-python google-cloud-sdk-app-engine-python-extras
 
-
-echo 'here'
-eval '@'
+dev_appserver.py . &
+ls -al
+eval "$@"
